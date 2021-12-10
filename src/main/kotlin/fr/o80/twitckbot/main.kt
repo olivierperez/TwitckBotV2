2d469b137e413c404fca3b9ab23bbd7af592a1d9
Olivier PEREZ <olivier@olivierperez.fr>
2021-12-10 15:43:58 +0100

    upgrade dependencies

--- src/main/kotlin/fr/o80/twitckbot/main.kt
+++ src/main/kotlin/fr/o80/twitckbot/main.kt
@@ -1,13 +1,18 @@
 package fr.o80.twitckbot
 
 import androidx.compose.ui.window.singleWindowApplication
-import com.arkivanov.decompose.extensions.compose.jetbrains.rememberRootComponent
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import fr.o80.twitckbot.navigation.NavHostComponent
 import fr.o80.twitckbot.theme.TwitckBotTheme
 
-fun main() = singleWindowApplication(title = "TwitckBot v2.0") {
-    TwitckBotTheme {
-        rememberRootComponent(factory = ::NavHostComponent)
-            .render()
+fun main() {
+    val lifecycle = LifecycleRegistry()
+    val root = NavHostComponent(DefaultComponentContext(lifecycle))
+
+    singleWindowApplication(title = "TwitckBot v2.0") {
+        TwitckBotTheme {
+            root.render()
+        }
     }
 }
