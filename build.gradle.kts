10f37614bf2b005dbc0ac70a60fda60357966c7d
Olivier PEREZ <olivier@olivierperez.fr>
2021-12-10 19:38:39 +0100

    chore(dependencies): upgrade dependencies (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,16 @@ plugins {
     kotlin("jvm") version "1.5.31"
     kotlin("kapt") version "1.5.31"
     kotlin("plugin.serialization") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0-alpha4-build398"
+    id("org.jetbrains.compose") version "1.0.0-beta5"
 }
 
 group = "fr.o80.twitckbot"
 version = "2.0"
 
 repositories {
-    jcenter()
     mavenCentral()
     google()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven("https://jitpack.io")
 }
 
@@ -25,32 +24,32 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
 
     // Decompose
-    implementation("com.arkivanov.decompose:decompose:0.2.6")
-    implementation("com.arkivanov.decompose:extensions-compose-jetbrains:0.2.6")
+    implementation("com.arkivanov.decompose:decompose:0.4.0")
+    implementation("com.arkivanov.decompose:extensions-compose-jetbrains:0.4.0")
 
     // Oauth: Ktor + OauthClient + HttpClient
-    implementation("io.ktor:ktor-server-core:1.3.0")
-    implementation("io.ktor:ktor-server-netty:1.3.0")
+    implementation("io.ktor:ktor-server-core:1.6.6")
+    implementation("io.ktor:ktor-server-netty:1.6.6")
     implementation("com.github.mazine:oauth2-client-kotlin:1.0.2")
-    implementation("org.glassfish.jersey.media:jersey-media-moxy:2.13")
-    implementation("org.apache.httpcomponents:httpclient:4.5.2")
+    implementation("org.glassfish.jersey.media:jersey-media-moxy:2.33")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
 
     // Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
 
     // Dependency injection
-    implementation("com.google.dagger:dagger:2.39.1")
-    kapt("com.google.dagger:dagger-compiler:2.39.1")
+    implementation("com.google.dagger:dagger:2.40.3")
+    kapt("com.google.dagger:dagger-compiler:2.40.3")
 
     // IRC
     implementation("pircbot:pircbot:1.5.0")
 
     // Log
-    implementation("ch.qos.logback:logback-classic:1.0.13")
+    implementation("ch.qos.logback:logback-classic:1.2.7")
 
     // Test
-    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("io.mockk:mockk:1.12.1")
     testImplementation(kotlin("test"))
 }
 
